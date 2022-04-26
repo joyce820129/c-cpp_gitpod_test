b36610a22d1b32a1b15326dd9ee7e2771b646b3f
joyce820129 <104434994+joyce820129@users.noreply.github.com>
2022-04-26 21:52:21 +0800

    Create ex1-2.cpp

---
+++ ex1-2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    printf("Hello World \n");
+    
+    cout <<  "hello Joyce \n";
+    cout <<  "my height is " << 1 << "m " << 61 << "cm\n";
+    cout <<  "my weight is " << 50 << "kg ";
+
+    return 0;
+}
