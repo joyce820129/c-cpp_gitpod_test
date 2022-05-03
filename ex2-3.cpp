8b7f59635475a716e6fa4aef5f52147438da1a20
joyce820129 <104434994+joyce820129@users.noreply.github.com>
2022-05-03 21:43:22 +0800

    Create ex2-3.cpp

---
+++ ex2-3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int   i;
+    int   j;
+    
+    
+    printf(" 請輸入第一個整數 \n");
+    scanf("%d",&i);
+    
+    
+    printf(" 請輸入第二個整數 \n");
+    scanf("%d",&j);
+    
+    
+    printf(" 兩數的平方合為= %d \n",j*j+i*i);
+    
+    return 0;
+}
