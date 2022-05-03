689bd84ec85f22eb2e5de8c9f9826ffd8a39e28a
joyce820129 <104434994+joyce820129@users.noreply.github.com>
2022-05-03 21:44:03 +0800

    Create ex2-4.cpp

---
+++ ex2-4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    
+    
+    
+    printf(" 請輸入一個圓半徑 \n");
+    scanf("%d",&i);
+    
+    printf(" 圓面積 = %f \n",i*i*3.14);
+    printf(" 圓周長 = %f \n",i*2*3.14);
+    
+    return 0;
+}
