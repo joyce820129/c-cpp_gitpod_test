bfd46a33ca520dd8cc734d14f7381f747ffb3323
joyce820129 <104434994+joyce820129@users.noreply.github.com>
2022-05-10 22:24:22 +0800

    Create ex3-3.cpp

---
+++ ex3-3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+    printf("please input x: \n");
+    scanf("%d",&x);
+    
+    printf("please input y: \n");
+    scanf("%d",&y);
+    
+    printf("  %d == %d = %d \n ",x,y,x==y);
+    printf(" %d != %d = %d \n ",x,y,x!=y);
+    printf(" %d > %d = %d \n ",x,y,x>y);
+    printf(" %d < %d = %d \n ",x,y,x<y);
+    printf(" %d >= %d = %d \n ",x,y,x>=y);
+    printf(" %d <= %d = %d \n ",x,y,x<=y);
+    
+    
+    
+}
