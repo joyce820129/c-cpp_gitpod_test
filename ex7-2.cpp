33fcc8c21fafa08408c82f4024300b4339f9bc1c
joyce820129 <joyce820129@gmail.com>
2022-06-07 12:47:14 +0000

    update

--- ex7-2.cpp
+++ ex7-2.cpp
@@ -6,6 +6,14 @@ C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, S
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <stdio.h>
 int addNumbers(int a, int b); // function prototype
 int takeNumbers(int a, int b);
@@ -15,19 +23,20 @@ int multNumbers(int a, int b);
 int main()
 {
     int n1,n2,sum;
-    printf("Enters two numbers: ");
-    scanf("%d %d",&n1,&n2);
+    printf("Enters two numbers: \n");
+    scanf("%d",&n1);
+    scanf("%d",&n2);
     sum = addNumbers(n1, n2);  // function call
-    printf("a+b = %d \n",sum);
+    printf("相加 = %d \n",sum);
     
     sum = takeNumbers(n1, n2);  // function call
-    printf("a-b = %d \n",sum);
+    printf("相減 = %d \n",sum);
     
     sum = multNumbers(n1, n2);  // function call
-    printf("a*b = %d \n",sum);
+    printf("相乘 = %d \n",sum);
     
     sum = dividNumbers(n1, n2);  // function call
-    printf("a/b = %d \n",sum);
+    printf("相除 = %d \n",sum);
     return 0;
 }
 
@@ -44,7 +53,7 @@ int addNumbers(int a, int b)         // function definition
 int takeNumbers(int a, int b)         // function definition   
 {
     int result;
-    result = a-b;
+    result = (a>b)? (a-b): (b-a);
     return result;                     
     //return statement
 }
@@ -52,7 +61,7 @@ int takeNumbers(int a, int b)         // function definition
 int dividNumbers(int a, int b)         // function definition   
 {
     int result;
-    result = a/b;
+    result = (a>b)? (a/b): (b/a);
     return result;                     
     //return statement
 }
