672e6fe97fcb026651082548a9502d3a090d320e
joyce820129 <joyce820129@gmail.com>
2022-06-07 13:04:16 +0000

    update

---
+++ ex7-4.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int timetotal(int hour, int min); 
+
+int main()
+{
+    int hour,min,mintotal;
+    printf("小時= ");
+    scanf("%d",&hour);
+    printf("分鐘= ");
+    scanf("%d",&min);
+
+    mintotal = timetotal(hour, min);  
+    printf("總共 %d 分鐘 \n",mintotal);
+    
+}
+
+int timetotal(int a, int b)          
+{
+    int result;
+    result = (a*60)+b;
+    return result;                     
+}
